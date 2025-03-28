eebcf368bb186afff6df243607eb83f2636e8348
Anvesh Shrivastava <itsofficialanvesh@gmail.com>
2025-03-28 13:40:10 +0530

    image is now showing

---
+++ src/backend/ClientHandler.java
@@ -0,0 +1,18 @@
+package backend;
+
+import java.net.Socket;
+
+public class ClientHandler
+{
+//	String returnAddress;
+
+	public ClientHandler(Socket socket)
+	{
+		// handle client requests
+	}
+
+	public void start()
+	{
+		// start handling client requests
+	}
+}
--- src/backend/Main.java
+++ src/backend/Main.java
@@ -1,6 +1,31 @@
 package backend;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Main
 {
+	public static int PORT = 8081;
+	public static void main(String[] args)
+	{
 
+	}
+
+	ServerSocket serverSocket;
+
+	Main()
+	{
+		try
+		{
+			ServerSocket serverSocket = new ServerSocket(PORT);
+			while (true)
+			{
+				new ClientHandler(serverSocket.accept()).start();
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
